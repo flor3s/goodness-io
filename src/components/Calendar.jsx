f046e37f763fe12427f50f707edcffeefddacd71
Daniel Flores <d.flores042@gmail.com>
2019-04-03 19:16:45 -0400

    update calendar component and styling

---
+++ src/components/Calendar.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import dateFns from "date-fns";
+
+class Calendar extends Component {
+  state = {
+    currentMonth: new Date(),
+    selectedDate: new Date()
+  };
+
+  renderHeader() {}
+  renderDays() {}
+  renderCells() {}
+
+  onDateClick = day => {};
+  nextMonth = () => {};
+  prevMonth = () => {};
+
+  render() {
+    return (
+      <div className="calendar">
+        {this.renderHeader()}
+        {this.renderDays()}
+        {this.renderCells()}
+      </div>
+    );
+  }
+}
+
+export default Calendar;
