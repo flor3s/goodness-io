396227febb4c15e8c955e8282c0539b4f1add223
Daniel Flores <d.flores042@gmail.com>
2019-04-05 11:29:44 -0400

    lift selectedDate to App component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, { Component } from "react";
 import Calendar from "./components/Calendar";
+import dateFns from "date-fns";
 import "./App.css";
 
 class App extends Component {
+  state = {
+    selectedDate: new Date()
+  };
+
+  onDateClick = day => {
+    console.log(day);
+    this.setState({
+      selectedDate: day
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header>
           <div id="logo">
-            <span className="icon">date_range</span>
+            <span className="icon">face</span>
             <span>
-              react<b>calendar</b>
+              <b>goodness</b>.io
             </span>
           </div>
         </header>
-        <main>Calendar</main>
+        <main>
+          <Calendar
+            selectedDate={this.state.selectedDate}
+            onDateClick={this.onDateClick}
+          />
+        </main>
       </div>
     );
   }
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,17 +3,107 @@ import dateFns from "date-fns";
 
 class Calendar extends Component {
   state = {
-    currentMonth: new Date(),
-    selectedDate: new Date()
+    currentMonth: new Date()
   };
 
-  renderHeader() {}
-  renderDays() {}
-  renderCells() {}
+  renderHeader() {
+    const dateFormat = "MMMM YYYY";
 
-  onDateClick = day => {};
-  nextMonth = () => {};
-  prevMonth = () => {};
+    return (
+      <div className="header row flex-middle">
+        <div className="col col-start">
+          <div className="icon" onClick={this.prevMonth}>
+            chevron_left
+          </div>
+        </div>
+        <div className="col col-center">
+          <span>{dateFns.format(this.state.currentMonth, dateFormat)}</span>
+        </div>
+        <div className="col col-end" onClick={this.nextMonth}>
+          <div className="icon">chevron_right</div>
+        </div>
+      </div>
+    );
+  }
+  renderDays() {
+    const dateFormat = "dddd";
+    const days = [];
+
+    let startDate = dateFns.startOfWeek(this.state.currentMonth);
+
+    for (let i = 0; i < 7; i++) {
+      days.push(
+        <div className="col col-center" key={i}>
+          {dateFns.format(dateFns.addDays(startDate, i), dateFormat)}
+        </div>
+      );
+    }
+
+    return <div className="days row">{days}</div>;
+  }
+
+  renderCells() {
+    const { currentMonth, selectedDate } = this.state;
+    const monthStart = dateFns.startOfMonth(currentMonth);
+    const monthEnd = dateFns.endOfMonth(monthStart);
+    const startDate = dateFns.startOfWeek(monthStart);
+    const endDate = dateFns.endOfWeek(monthEnd);
+
+    const dateFormat = "D";
+    const rows = [];
+    const selected = this.props.selectedDate;
+
+    let days = [];
+    let day = startDate;
+    let formattedDate = "";
+
+    let dayClass = day => {
+      if (dateFns.isSameDay(day, selected)) {
+        return "selected";
+      } else if (!dateFns.isSameMonth(day, monthStart)) {
+        return "disabled";
+      } else {
+        return "";
+      }
+    };
+
+    while (day <= endDate) {
+      for (let i = 0; i < 7; i++) {
+        formattedDate = dateFns.format(day, dateFormat);
+        const cloneDay = day;
+
+        days.push(
+          <div
+            className={`col cell ${dayClass(cloneDay)}`}
+            key={day}
+            onClick={() => this.props.onDateClick(dateFns.parse(cloneDay))}
+          >
+            <span className="number">{formattedDate}</span>
+          </div>
+        );
+        day = dateFns.addDays(day, 1);
+      }
+      rows.push(
+        <div className="row" key={day}>
+          {days}
+        </div>
+      );
+      days = [];
+    }
+
+    return <div className="body">{rows}</div>;
+  }
+
+  nextMonth = () => {
+    this.setState({
+      currentMonth: dateFns.addMonths(this.state.currentMonth, 1)
+    });
+  };
+  prevMonth = () => {
+    this.setState({
+      currentMonth: dateFns.subMonths(this.state.currentMonth, 1)
+    });
+  };
 
   render() {
     return (
